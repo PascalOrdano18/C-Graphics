aa68d49e4d50d830fbfb38bb461a2a77ee2ae2e5
Pascal Ordano <pascalordanodev@gmail.com>
2025-12-24 12:19:17 -0300

    already ploting

--- plot/plot.c
+++ plot/plot.c
@@ -1,22 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL2/SDL.h>
+#include <math.h>
 
 #define WIDTH 900
-#define HEIGHT 600
+#define HEIGHT 900
 
-#define COLOR_WHITE 0xFFFFFFFF;
+#define COLOR_WHITE 0xFFFFFFFF
+#define COLOR_GRAY 0x88888888
+
+void plot_normalized_point(SDL_Surface* surface, double x, double y);
+
+void draw_axis(SDL_Surface* surface){
+    int x = WIDTH / 2;
+    int y = HEIGHT / 2;
+    for(int i = 0; i < WIDTH; i++){
+        SDL_Rect point = (SDL_Rect) { i, y, 1, 1 };
+        SDL_FillRect(surface, &point, COLOR_GRAY);
+   
+    }
+    for(int i = 0; i < HEIGHT; i++){
+        SDL_Rect point = (SDL_Rect) { x, i, 1, 1 };
+        SDL_FillRect(surface, &point, COLOR_GRAY);
+    }
+}
 
 // sin(x) + 10 + x**2 / x * cos(x)
-void draw_function(){
-    for(int x = 0; x < WIDTH; x++){
-        
+void draw_function(SDL_Surface* surface){
+    for(double x = -WIDTH/2; x < WIDTH / 2; x+=0.1){
+        double y = x*x;
+        plot_normalized_point(surface, x, y);
     }
+}
+
+void plot_normalized_point(SDL_Surface* surface, double x, double y){
+    double nx = x + WIDTH / 2;
+    double ny = -y + HEIGHT / 2;
+    SDL_Rect point = (SDL_Rect) { nx, ny, 1, 1 };
+    SDL_FillRect(surface, &point, COLOR_WHITE);
 }
 
 
 int main(int argc, char* argv[]){
-    if(argc < 2) return 0; // necesitamos que nos pasen datos de la funcion
+    // if(argc < 2) return 0; // necesitamos que nos pasen datos de la funcion
+
 
    SDL_Init(SDL_INIT_VIDEO);
    SDL_Window* window = SDL_CreateWindow("Function Plotter", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
@@ -33,7 +60,8 @@ int main(int argc, char* argv[]){
            }
        }
 
-       draw_function();
+       draw_axis(surface);
+       draw_function(surface);
 
        SDL_UpdateWindowSurface(window);
    }
