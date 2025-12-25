027e9f6bb2c284fb3c4005027d87de9b5737351e
Pascal Ordano <pascalordanodev@gmail.com>
2025-12-25 12:43:31 -0300

    asdaf

--- sand/figures.c
+++ sand/figures.c
@@ -4,25 +4,37 @@
 #include <time.h>
 
 
+#define COLORS 4
+#define COLOR_WHITE 0xFFFFFFFF
+#define COLOR_BLUE 0xFF0000FF
+#define COLOR_RED 0xFFFF0000
+#define COLOR_GREEN 0xFF00FF00
 #define BLOCK 20
 
 typedef struct{
     int x;
     int y;
-    int shape;  // 0 a 6, con I, T, L, S, Z, O (cuadrado)
+    int shape;  // 0 a 5, con I, T, L, S, Z, O (cuadrado), 
     int rot; // de 0 a 3
     Uint32 color;
     char active; // 0 si no esta cayendo, 1 si
 } Piece;
 
+static Uint32 colors = {COLOR_WHITE, COLOR_BLUE, COLOR_GREEN, COLOR_RED};
 
-void piece_spawn(){
-   srand((unsigned)time(NULL));
+Piece piece_spawn(){
+    srand((unsigned)time(NULL));
 
-   int random_piece = rand() % 7;   // 7 porque hay 6 piezas
+    int shape = rand() % 7;   // 7 porque hay 6 piezas
+    Uint32 color = colors[rand() % COLORS];
+   
+    Piece piece = { 100, 100, shape, 0, color, 1 };
     
-
+    return piece;
 }
+
+
+
 
 
 
