653913dc46e8d6ae7c488d98381f8c4f001a7726
Pascal Ordano <pascalordanodev@gmail.com>
2025-12-27 22:36:35 -0300

    jump

--- dino/dino.c
+++ dino/dino.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL2/SDL.h>
+#include <time.h>
 
 #define WIDTH 900
 #define HEIGHT 600
 #define TAMANO 20
+#define PISO 450 + TAMANO
 
 #define COLOR_WHITE 0xFFFFFFFF
 #define COLOR_GRAY 0xFF888888
@@ -17,11 +19,26 @@ typedef struct {
     Uint32 color;
 } Dino;
 
+typedef struct {
+    int x;
+    int y;
+    int height;
+    Uint32 color;
+} Obstacle;
+
 void draw_dino(SDL_Surface* surface, Dino dinosaurio){
     SDL_Rect dino_rect = { dinosaurio.x, dinosaurio.y, dinosaurio.tamano, dinosaurio.tamano }; 
     SDL_FillRect(surface, &dino_rect, dinosaurio.color); 
 }
 
+
+void draw_obstacles(SDL_Surface* surface){
+    int height = 10 +  rand() % 30; // altura minima 10, maxima 40
+                                  //
+    Obstacle obstacle = { 500, PISO - height, height, COLOR_GRAY };
+    SDL_Rect obs = (SDL_Rect) { obstacle.x, obstacle.y, 20, height };
+    SDL_FillRect(surface, &obs, obstacle.color);
+}
 
 
 int main(void){
@@ -29,6 +46,8 @@ int main(void){
     SDL_Window* window = SDL_CreateWindow("Dino", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
 
     SDL_Surface* surface = SDL_GetWindowSurface(window);
+
+    srand(time(NULL));
 
     const Uint32 RENDER_MS = 16;
     const Uint32 SIM_MS = 20;
@@ -42,6 +61,11 @@ int main(void){
         TAMANO,
         COLOR_WHITE
     };
+
+    int vy = 0;
+    int gravity = 1;
+    int jumpSpeed = -12;
+
     SDL_Rect piso = { 0, dinosaurio.y + dinosaurio.tamano, WIDTH, HEIGHT - (dinosaurio.y + dinosaurio.tamano) };
     SDL_FillRect(surface, &piso, COLOR_GRAY);
 
@@ -65,28 +89,25 @@ int main(void){
             if(event.type == SDL_KEYDOWN){
                 SDL_Keycode key = event.key.keysym.sym;
                 if(key == SDLK_SPACE){
-                    if(dinosaurio.y == 450){
-                        while(dinosaurio.y >= 400){
-                            dinosaurio.y -= 1; 
-                            draw_dino(surface, dinosaurio);
-                        }
-                    }
-                   
+                    if(dinosaurio.y == 450) vy = jumpSpeed;
                 }
             }
         }
 
         while(acc >= SIM_MS){
-            if(dinosaurio.y <= 400){
-                while(dinosaurio.y < 450){
-                    dinosaurio.y += 1;
-                    draw_dino(surface, dinosaurio);
-                }
+            vy += gravity;
+            dinosaurio.y += vy;
+            if(dinosaurio.y >= 450){
+                dinosaurio.y = 450;
+                vy = 0;
             } 
+
+            SDL_FillRect(surface, NULL, COLOR_BLACK);
+            SDL_FillRect(surface, &piso, COLOR_GRAY);
             draw_dino(surface, dinosaurio);
+            draw_obstacles(surface);
             acc -= SIM_MS;
         }
-        SDL_FillRect(surface, &erase_rect, COLOR_BLACK);
         SDL_UpdateWindowSurface(window);
     }
 
